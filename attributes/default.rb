115e68d9813a4106119c5080acceef43e4524cb2
raymer <AndrewRaymer@gmail.com>
2014-04-06 11:20:59 -0400

    Refactored the way the cookbook works. It is now a client or server model

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['zabbix']['ipaddress'] = "10.13.37.24"
-default['zabbix']['config'] = "/etc/zabbix/"
-default['zabbix']['service'] = "zabbix-agent"
+default['zabbix']['client']['ipaddress'] = "10.13.37.24"
+default['zabbix']['client']['config'] = "/etc/zabbix/"
+default['zabbix']['client']['service'] = "zabbix-agent"
