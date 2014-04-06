af6bf387fb0f9ab08f306c58e2b900ee5591344a
raymer <AndrewRaymer@gmail.com>
2014-04-06 11:21:27 -0400

    Added the client.rb recipe

---
+++ recipes/client.rb
@@ -0,0 +1,15 @@
+package "#{node['zabbix']['client']['service']}" do
+    action :install
+end
+
+template "#{node['zabbix']['client']['config']}/zabbix_agentd.conf" do
+    source "zabbix_agentd.conf.erb"
+    mode "0644"
+    owner = "root"
+    group = "root"
+    notifies :restart, "service[zabbix-agent]", :immediately
+end
+
+service "#{node['zabbix']['client']['service']}" do
+    action [ :start, :enable ]
+end
